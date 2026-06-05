2ae8e5b9dbfed1278db42ca56057677bc2a6b93f
12m284 <18127725754@163.com>
2026-06-05 16:39:21 +0800

    Add files via upload

---
+++ 俄罗斯方块/Tetris.cpp
@@ -0,0 +1,6 @@
+#include "Tetris.h"
+
+Tetris::Tetris(int row, int clos, int left, int top, int blockSize)
+{
+}
+
---
+++ 俄罗斯方块/Tetris.h
@@ -0,0 +1,10 @@
+#pragma once
+class Tetris
+{
+public:
+
+	Tetris(int row, int clos, int left, int top, int blockSize);
+	void init();
+	void play();
+	Tetris();
+};
