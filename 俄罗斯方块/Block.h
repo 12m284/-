fcd023b784b5c73349575db0b3005cab26c2760c
12m284 <18127725754@163.com>
2026-06-05 20:14:04 +0800

    Update Block.h

--- 俄罗斯方块/Block.h
+++ 俄罗斯方块/Block.h
@@ -1,5 +1,12 @@
 #pragma once
+
 class Block
 {
+public:
+    Block();
+    void drop();
+    void movveLeftRight(int offest);
+    void retate();
+    voide draw(int leftMargin, int topMargin);
 };
 
