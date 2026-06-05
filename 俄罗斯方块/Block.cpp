124272e9731dd30c465a2bdf40485028f060ce89
12m284 <18127725754@163.com>
2026-06-05 22:15:03 +0800

    Update Block.cpp

--- 俄罗斯方块/Block.cpp
+++ 俄罗斯方块/Block.cpp
@@ -1,7 +1,23 @@
 #include "Block.h"
+#include <stdlib.h>
+
+
 
 Block::Block()
 {
+int block[7][4] = {
+    1,3,5,7,//I
+    2,4,5,7,
+    3,5,4,6,
+    3,5,4,7,
+    2,3,5,7,
+    3,5,7,6,
+    2,3,4,5,
+};
+
+//随机生成一种俄罗斯方块
+  rand() % 7;
+  
 }
 
 void Block::drop()
