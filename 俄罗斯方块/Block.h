2a841efde75c8b9f1c9fa122a2764662e0e8decd
12m284 <18127725754@163.com>
2026-06-06 17:34:45 +0800

    Update Block.h

--- 俄罗斯方块/Block.h
+++ 俄罗斯方块/Block.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <graphcs.h> //需要安装easyx图形库
 
 struct Point {
      int row;
@@ -15,9 +16,13 @@ public:
     void moveLeftRight(int offest);
     void retate();//旋转
     voide draw(int leftMargin, int topMargin);
-     
+
 private:
 int blockType; //方块的类型
-Points smallBlocks[4];
+Point smallBlocks[4];
+IMAGE *img;
+
+static IMAGE* imgs[7]:
+static int size;
 };
 
