654b5ffb0853a8dfd1dfcfde2b5d54fa2bca05e1
12m284 <18127725754@163.com>
2026-06-07 19:48:49 +0800

    Update Tetris.h

--- 俄罗斯方块/Tetris.h
+++ 俄罗斯方块/Tetris.h
@@ -4,12 +4,15 @@ class Tetris
 public:
 
 	Tetris(int row, int clos, int left, int top, int blockSize);
-	void init();
-	void play();
+	void init();//初始化
+	void play();//开始游戏
 
 private:
     void keyEvent();
     void updateWindow();
+
+//返回距离上次调用该函数，间隔了多少时间（ms)
+//第一次调用该函数，返回0
     int getDelay();
     void drop();
     voide clearLine();
